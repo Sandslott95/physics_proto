e339445b9bdb5c2b6205bff6b1691ba13e7fd6b9
Nils.M <nimr20@student.bth.se>
2022-04-08 14:08:18 +0200

    Added operator overloading for vector2

--- physics_proto/Vector.hpp
+++ physics_proto/Vector.hpp
@@ -83,6 +83,69 @@ struct Vector2 {
 
 		return ret;
 	}
+
+	Vector2& operator-=(const Vector2& other) {
+
+		this->x -= other.x;
+		this->y -= other.y;
+		return *this;
+	}
+	Vector2& operator+=(const Vector2& other) {
+		this->x += other.x;
+		this->y += other.y;
+		return *this;
+	}
+	Vector2& operator*=(const Vector2& other) {
+		this->x *= other.x;
+		this->y *= other.y;
+		return *this;
+	}
+	Vector2& operator/=(const Vector2& other) {
+		this->x /= other.x;
+		this->y /= other.y;
+		return *this;
+	}
+	Vector2& operator-=(const float& other) {
+		this->x -= other;
+		this->y -= other;
+		return *this;
+	}
+	Vector2& operator+=(const float& other) {
+		this->x += other;
+		this->y += other;
+		return *this;
+	}
+	Vector2& operator*=(const float& other) {
+		this->x *= other;
+		this->y *= other;
+		return *this;
+	}
+	Vector2& operator/=(const float& other) {
+		this->x /= other;
+		this->y /= other;
+		return *this;
+	}
+	Vector2& operator-=(const int& other) {
+		this->x -= other;
+		this->y -= other;
+		return *this;
+	}
+	Vector2& operator+=(const int& other) {
+		this->x += other;
+		this->y += other;
+		return *this;
+	}
+	Vector2& operator*=(const int& other) {
+		this->x *= other;
+		this->y *= other;
+		return *this;
+	}
+	Vector2& operator/=(const int& other) {
+		this->x /= other;
+		this->y /= other;
+		return *this;
+	}
+
 };
 
 struct Vector3 {
